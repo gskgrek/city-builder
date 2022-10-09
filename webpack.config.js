7c01dcd8e8fac0fea80df4f4d646300f20c2139d
gskgrek <biuro@greku.pl>
2022-10-09 14:03:53 +0200

    webpack dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'City Builder',
