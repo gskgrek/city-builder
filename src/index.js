035e7ed3ef9a409c4332cc4f8ed1da735abcdc93
gskgrek <biuro@greku.pl>
2022-10-09 13:54:57 +0200

    webpack assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import './index.css'
+import Logo from './assets/images/logo.png'
+
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('div')
 
-  element.innerHTML = 'Hello World';
+  element.innerHTML = 'Hello World'
+  element.classList.add('hello')
 
-  return element;
+  const logo = new Image()
+  logo.src = Logo
+
+  element.appendChild(logo)
+
+  return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
